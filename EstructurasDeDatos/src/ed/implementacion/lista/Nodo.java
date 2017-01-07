949b7590c846ee55262e70bfa15dfc92d90fffb3
Omar Mendoza <omar.mendoza.bernal@gmail.com>
2017-01-07 00:54:10 -0600

    astyle a listas

--- EstructurasDeDatos/src/ed/implementacion/lista/Nodo.java
+++ EstructurasDeDatos/src/ed/implementacion/lista/Nodo.java
@@ -1,28 +1,28 @@
 package ed.implementacion.lista;
 
 public class Nodo<T> {
-	private T valor;
-	private Nodo<T> siguiente;
-	
-	public Nodo() {
-		this.valor = null;
-		this.siguiente = null;
-	}
+    private T valor;
+    private Nodo<T> siguiente;
 
-	public T getValor() {
-		return valor;
-	}
+    public Nodo() {
+        this.valor = null;
+        this.siguiente = null;
+    }
 
-	public void setValor(T valor) {
-		this.valor = valor;
-	}
+    public T getValor() {
+        return valor;
+    }
 
-	public Nodo<T> getSiguiente() {
-		return siguiente;
-	}
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
 
-	public void setSiguiente(Nodo<T> siguiente) {
-		this.siguiente = siguiente;
-	}
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
 }
 
--- EstructurasDeDatos/src/ed/implementacion/lista/PruebaLista.java
+++ EstructurasDeDatos/src/ed/implementacion/lista/PruebaLista.java
@@ -1,25 +1,25 @@
 package ed.implementacion.lista;
 
 public class PruebaLista {
-	
-	public static void main(String[] args) {
-		
-		// Lista de Enteros
-		Lista<Integer> lista = new Lista<Integer>();		
-		for(int i = 0; i < 5; i++)
-			lista.agregarAlFinal(i);
-		
-		System.out.println("Tamaño de la lista: " + lista.getTamanio());
-		System.out.println("Lista:");
-		lista.mostrar();
-		
-		// Lista de Doubles
-		System.out.println("\n\nLista Doubles");
-		Lista<Double> listaDoubles = new Lista<>();
-		listaDoubles.agregarAlInicio(1.0);
-		listaDoubles.agregarAlInicio(2.0);
-		listaDoubles.agregarAlInicio(3.0);
-		listaDoubles.mostrar();
-	}
+
+    public static void main(String[] args) {
+
+        // Lista de Enteros
+        Lista<Integer> lista = new Lista<Integer>();
+        for(int i = 0; i < 5; i++)
+            lista.agregarAlFinal(i);
+
+        System.out.println("Tamaño de la lista: " + lista.getTamanio());
+        System.out.println("Lista:");
+        lista.mostrar();
+
+        // Lista de Doubles
+        System.out.println("\n\nLista Doubles");
+        Lista<Double> listaDoubles = new Lista<>();
+        listaDoubles.agregarAlInicio(1.0);
+        listaDoubles.agregarAlInicio(2.0);
+        listaDoubles.agregarAlInicio(3.0);
+        listaDoubles.mostrar();
+    }
 
 }
